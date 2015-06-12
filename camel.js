2260ed4bf5a6ef964f162b14effe86a6a0334138
Jonathon Choo <jonno.choo@gmail.com>
2015-06-12 11:18:16 +1000

    Fixed issue with camelizing arrays

--- camel.js
+++ camel.js
@@ -1,38 +1,44 @@
-;(function(global) {
-    
+; (function (global) {
+
     var camel = {
         camelize: camelize
-    };    
+    };
 
-  function camelize(obj){
-    var newObj = {};
+    function camelize(obj) {
+        var newObj = {};
+        if (obj.constructor === Array) {
+            newObj = [];
+            for (var i = 0; i < obj.length; i++) {
+                newObj.push(camelize(obj[i]));
+            }
+        } else {
+            for (var propertyName in obj) {
 
-    for(var propertyName in obj) {
-      
-      var property = obj[propertyName];
-      var camelCasePropertyName = camelizeText(propertyName);
+                var property = obj[propertyName];
+                var camelCasePropertyName = camelizeText(propertyName);
 
-      if(property !== null && typeof(property) === 'object') {
-        newObj[camelCasePropertyName] = camelize(property)
-      } else {
-        newObj[camelCasePropertyName] = property;  
-      }
+                if (property !== null && typeof(property) === 'object') {
+                    newObj[camelCasePropertyName] = camelize(property);
+                } else {
+                    newObj[camelCasePropertyName] = property;
+                }
 
+            }
+        }
+
+        return newObj;
     }
 
-    return newObj;
-  }
+    function camelizeText(text) {
+        return text.replace(/^([A-Z])/, function (match, c) {
+            return c ? c.toLowerCase() : '';
+        });
+    }
 
-  function camelizeText(text){
-    return text.replace(/^([A-Z])/, function(match, c) {
-        return c ? c.toLowerCase() : '';
-      });
-  }
-  
-  if (typeof module !== 'undefined' && module.exports) {
-      module.exports = camel;
-  } else {
-      global.camel = camel;
-  }
-    
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = camel;
+    } else {
+        global.camel = camel;
+    }
+
 })(this);
